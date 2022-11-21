33ad1474a06a2d6db89daa0350665a283eabb3ec
Emely <Emely@LAPTOP-41V0NP9T>
2022-11-20 23:14:47 -0400

    Constructor de la Clase CoordinacionEvento

--- ProyectoFinalPoo/src/logico/CoordinacionEvento.java
+++ ProyectoFinalPoo/src/logico/CoordinacionEvento.java
@@ -9,5 +9,65 @@ public class CoordinacionEvento {
 	private ArrayList<TrabajoCientifico> trabajosCientificos;
 	private ArrayList<Recurso> recursos;
 	
+	private static CoordinacionEvento coordinacionEventos = null;
 
+	public CoordinacionEvento() {
+		super();
+		this.eventos = new ArrayList<>();
+		this.personas = new ArrayList<>();
+		this.trabajosCientificos =new ArrayList<>();
+		this.recursos = new ArrayList<>();
+	}
+
+	public static CoordinacionEvento getInstance(){
+		if(coordinacionEventos == null){
+			coordinacionEventos = new CoordinacionEvento();  
+		} 	   
+		return coordinacionEventos;
+	}
+	
+	public ArrayList<Evento> getEventos() {
+		return eventos;
+	}
+
+	public void setEventos(ArrayList<Evento> eventos) {
+		this.eventos = eventos;
+	}
+
+	public ArrayList<Persona> getPersonas() {
+		return personas;
+	}
+
+	public void setPersonas(ArrayList<Persona> personas) {
+		this.personas = personas;
+	}
+
+	public ArrayList<TrabajoCientifico> getTrabajosCientificos() {
+		return trabajosCientificos;
+	}
+
+	public void setTrabajosCientificos(ArrayList<TrabajoCientifico> trabajosCientificos) {
+		this.trabajosCientificos = trabajosCientificos;
+	}
+
+	public ArrayList<Recurso> getRecursos() {
+		return recursos;
+	}
+
+	public void setRecursos(ArrayList<Recurso> recursos) {
+		this.recursos = recursos;
+	}
+	
+	public void insertarFactura(Evento evento) {
+		eventos.add(evento);	
+	}
+	public void insertarFactura(Persona persona) {
+		personas.add(persona);
+	}
+	public void insertarFactura(TrabajoCientifico trabajo) {
+		trabajosCientificos.add(trabajo);
+	}
+	public void insertarFactura(Recurso recurso) {
+		recursos.add(recurso);
+	}
 }
