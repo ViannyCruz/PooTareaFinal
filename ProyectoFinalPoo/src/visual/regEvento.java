17d6532c42de0c499241ae25494b84be4dd4d85b
Vianny <cruzcruzvianny@gmail.com>
2022-12-02 18:13:02 -0400

    Actualizacion de regEvento.

--- ProyectoFinalPoo/src/visual/regEvento.java
+++ ProyectoFinalPoo/src/visual/regEvento.java
@@ -19,22 +19,40 @@ import javax.swing.JScrollPane;
 import javax.swing.JList;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JRadioButton;
 
 public class regEvento extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_8;
-	private JTextField textField_9;
-	private JTextField textField_10;
-	private JTextField textField_11;
+	private JTextField txtCodigo;
+	private JTextField txtNombre;
+	private JTextField txtTipo;
+	private JTextField txtTema;
+	private JTextField txtUbicacion;
+	private JTextField txtBarraBuscar;
+	private JTextField txtNombreDos;
+	private JTextField txtCedula;
+	private JTextField txtTelefono;
+	private JTextField txtEspecialidad;
+	private JTextField txtModeradorActual;
+	private JTextField txtAreaDeComision;
+	private JRadioButton rdbtnComision;
+	private JRadioButton rdbtnParticipantes;
+	private JRadioButton rdbtRecursos;
+	private JButton btnBuscar;
+	private JButton btnAgregarComoModerador;
+	private JButton btnAgregar;
+	private JButton btnEliminar;
+	private JLabel lblAreaDeLaComision;
+	private JLabel lblModeradorActual;
+	private JLabel lblEspecialidad;
+	private JLabel LBLaCTUALES;
+	private JLabel lblBuscar;
+	private JLabel lblTitulo;
+	private JButton btnAgregarParticipante;
+	private JButton btnEliminarParticipante;
+	private JButton btnAgregarRecurso;
+	private JButton btnEliminarRecurso;
 
 	/**
 	 * Launch the application.
@@ -53,6 +71,7 @@ public class regEvento extends JDialog {
 	 * Create the dialog.
 	 */
 	public regEvento() {
+		
 		setBounds(100, 100, 530, 700);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -72,51 +91,51 @@ public class regEvento extends JDialog {
 			lblNewLabel_1.setBounds(10, 36, 59, 20);
 			panel.add(lblNewLabel_1);
 			
-			textField = new JTextField();
-			textField.setBounds(70, 8, 163, 20);
-			panel.add(textField);
-			textField.setColumns(10);
+			txtCodigo = new JTextField();
+			txtCodigo.setBounds(70, 8, 163, 20);
+			panel.add(txtCodigo);
+			txtCodigo.setColumns(10);
 			
-			textField_1 = new JTextField();
-			textField_1.setBounds(70, 36, 401, 20);
-			panel.add(textField_1);
-			textField_1.setColumns(10);
+			txtNombre = new JTextField();
+			txtNombre.setBounds(70, 36, 401, 20);
+			panel.add(txtNombre);
+			txtNombre.setColumns(10);
 			
 			JLabel lblNewLabel_2 = new JLabel("Fecha:");
 			lblNewLabel_2.setBounds(260, 8, 46, 20);
 			panel.add(lblNewLabel_2);
 			
-			JSpinner spinner = new JSpinner();
-			spinner.setModel(new SpinnerDateModel(new Date(1669262400000L), null, null, Calendar.DAY_OF_YEAR));
-			spinner.setBounds(308, 8, 163, 20);
-			panel.add(spinner);
+			JSpinner spnFecha = new JSpinner();
+			spnFecha.setModel(new SpinnerDateModel(new Date(1669262400000L), null, null, Calendar.DAY_OF_YEAR));
+			spnFecha.setBounds(308, 8, 163, 20);
+			panel.add(spnFecha);
 			
 			JLabel lblNewLabel_3 = new JLabel("Tipo:");
 			lblNewLabel_3.setBounds(10, 68, 46, 20);
 			panel.add(lblNewLabel_3);
 			
-			textField_2 = new JTextField();
-			textField_2.setBounds(70, 68, 163, 20);
-			panel.add(textField_2);
-			textField_2.setColumns(10);
+			txtTipo = new JTextField();
+			txtTipo.setBounds(70, 68, 163, 20);
+			panel.add(txtTipo);
+			txtTipo.setColumns(10);
 			
 			JLabel lblNewLabel_4 = new JLabel("Tema:");
 			lblNewLabel_4.setBounds(260, 67, 46, 20);
 			panel.add(lblNewLabel_4);
 			
-			textField_3 = new JTextField();
-			textField_3.setBounds(308, 67, 163, 20);
-			panel.add(textField_3);
-			textField_3.setColumns(10);
+			txtTema = new JTextField();
+			txtTema.setBounds(308, 67, 163, 20);
+			panel.add(txtTema);
+			txtTema.setColumns(10);
 			
 			JLabel lblNewLabel_5 = new JLabel("Ubicacion:");
 			lblNewLabel_5.setBounds(10, 96, 82, 20);
 			panel.add(lblNewLabel_5);
 			
-			textField_4 = new JTextField();
-			textField_4.setBounds(70, 96, 401, 20);
-			panel.add(textField_4);
-			textField_4.setColumns(10);
+			txtUbicacion = new JTextField();
+			txtUbicacion.setBounds(70, 96, 401, 20);
+			panel.add(txtUbicacion);
+			txtUbicacion.setColumns(10);
 			
 			JPanel panel_1 = new JPanel();
 			panel_1.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -124,82 +143,89 @@ public class regEvento extends JDialog {
 			panel.add(panel_1);
 			panel_1.setLayout(null);
 			
-			textField_5 = new JTextField();
-			textField_5.setBounds(10, 25, 339, 22);
-			panel_1.add(textField_5);
-			textField_5.setColumns(10);
+			txtBarraBuscar = new JTextField();
+			txtBarraBuscar.setBounds(10, 25, 339, 22);
+			panel_1.add(txtBarraBuscar);
+			txtBarraBuscar.setColumns(10);
 			
-			JButton btnNewButton = new JButton("Buscar");
-			btnNewButton.setBounds(362, 24, 89, 23);
-			panel_1.add(btnNewButton);
+			btnBuscar = new JButton("Buscar");
+			btnBuscar.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					
+					
+				}
+			});
+			btnBuscar.setBounds(362, 24, 89, 23);
+			panel_1.add(btnBuscar);
 			
-			JLabel lblNewLabel_7 = new JLabel("Buscar jurado por cedula:");
-			lblNewLabel_7.setBounds(12, 0, 176, 22);
-			panel_1.add(lblNewLabel_7);
+			lblBuscar = new JLabel("Buscar jurado por cedula");
+			lblBuscar.setBounds(12, 0, 226, 22);
+			panel_1.add(lblBuscar);
 			
-			JButton btnNewButton_2 = new JButton("Agregar jurado");
-			btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
-			btnNewButton_2.setBounds(158, 240, 117, 23);
-			panel_1.add(btnNewButton_2);
+			btnAgregar = new JButton("Agregar jurado");
+			btnAgregar.setFont(new Font("Tahoma", Font.PLAIN, 12));
+			btnAgregar.setBounds(158, 240, 117, 23);
+			panel_1.add(btnAgregar);
 			
-			JButton btnNewButton_3 = new JButton("Eliminar jurado");
-			btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
-			btnNewButton_3.setBounds(285, 240, 117, 23);
-			panel_1.add(btnNewButton_3);
+			btnEliminar = new JButton("Eliminar jurado");
+			btnEliminar.setFont(new Font("Tahoma", Font.PLAIN, 12));
+			btnEliminar.setBounds(285, 240, 117, 23);
+			panel_1.add(btnEliminar);
 			
-			JLabel lblNewLabel_8 = new JLabel("Nombre:");
-			lblNewLabel_8.setBounds(158, 60, 80, 20);
-			panel_1.add(lblNewLabel_8);
+			JLabel lblNombreDos = new JLabel("Nombre:");
+			lblNombreDos.setBounds(158, 60, 191, 20);
+			panel_1.add(lblNombreDos);
 			
-			textField_6 = new JTextField();
-			textField_6.setBounds(158, 80, 293, 20);
-			panel_1.add(textField_6);
-			textField_6.setColumns(10);
+			txtNombreDos = new JTextField();
+			txtNombreDos.setBounds(158, 80, 293, 20);
+			panel_1.add(txtNombreDos);
+			txtNombreDos.setColumns(10);
 			
-			JLabel lblNewLabel_9 = new JLabel("Cedula:");
-			lblNewLabel_9.setBounds(158, 105, 46, 20);
-			panel_1.add(lblNewLabel_9);
+			JLabel lblCedula = new JLabel("Cedula:");
+			lblCedula.setBounds(158, 105, 46, 20);
+			panel_1.add(lblCedula);
 			
-			textField_7 = new JTextField();
-			textField_7.setBounds(158, 124, 191, 20);
-			panel_1.add(textField_7);
-			textField_7.setColumns(10);
+			txtCedula = new JTextField();
+			txtCedula.setBounds(158, 124, 191, 20);
+			panel_1.add(txtCedula);
+			txtCedula.setColumns(10);
 			
-			JLabel lblNewLabel_10 = new JLabel("Telefono:");
-			lblNewLabel_10.setBounds(158, 149, 67, 20);
-			panel_1.add(lblNewLabel_10);
+			JLabel lblTelefono = new JLabel("Telefono:");
+			lblTelefono.setBounds(158, 149, 67, 20);
+			panel_1.add(lblTelefono);
 			
-			textField_8 = new JTextField();
-			textField_8.setBounds(158, 167, 191, 20);
-			panel_1.add(textField_8);
-			textField_8.setColumns(10);
+			txtTelefono = new JTextField();
+			txtTelefono.setBounds(158, 167, 191, 20);
+			panel_1.add(txtTelefono);
+			txtTelefono.setColumns(10);
 			
-			JLabel lblNewLabel_11 = new JLabel("Especialidad:");
-			lblNewLabel_11.setBounds(158, 190, 122, 20);
-			panel_1.add(lblNewLabel_11);
+			lblEspecialidad = new JLabel("Especialidad:");
+			lblEspecialidad.setBounds(158, 190, 122, 20);
+			panel_1.add(lblEspecialidad);
 			
-			textField_9 = new JTextField();
-			textField_9.setBounds(158, 209, 191, 20);
-			panel_1.add(textField_9);
-			textField_9.setColumns(10);
+			txtEspecialidad = new JTextField();
+			txtEspecialidad.setBounds(158, 209, 191, 20);
+			panel_1.add(txtEspecialidad);
+			txtEspecialidad.setColumns(10);
 			
-			JButton btnNewButton_4 = new JButton("Agregar como Moderador");
-			btnNewButton_4.setFont(new Font("Tahoma", Font.PLAIN, 12));
-			btnNewButton_4.setBounds(158, 277, 244, 23);
-			panel_1.add(btnNewButton_4);
+			btnAgregarComoModerador = new JButton("Agregar como Moderador");
+			btnAgregarComoModerador.setFont(new Font("Tahoma", Font.PLAIN, 12));
+			btnAgregarComoModerador.setBounds(158, 277, 244, 23);
+			panel_1.add(btnAgregarComoModerador);
 			
-			JLabel lblNewLabel_12 = new JLabel("Moderador Actual:");
-			lblNewLabel_12.setBounds(10, 356, 122, 14);
-			panel_1.add(lblNewLabel_12);
+			lblModeradorActual = new JLabel("Moderador Actual:");
+			lblModeradorActual.setBounds(10, 356, 122, 14);
+			panel_1.add(lblModeradorActual);
 			
-			textField_10 = new JTextField();
-			textField_10.setBounds(10, 370, 441, 20);
-			panel_1.add(textField_10);
-			textField_10.setColumns(10);
+			txtModeradorActual = new JTextField();
+			txtModeradorActual.setBounds(10, 370, 441, 20);
+			panel_1.add(txtModeradorActual);
+			txtModeradorActual.setColumns(10);
 			
-			JLabel lblNewLabel_13 = new JLabel("Jurados actuales");
-			lblNewLabel_13.setBounds(10, 60, 117, 20);
-			panel_1.add(lblNewLabel_13);
+			LBLaCTUALES = new JLabel("Jurados actuales");
+			LBLaCTUALES.setBounds(10, 60, 138, 20);
+			panel_1.add(LBLaCTUALES);
 			
 			JScrollPane scrollPane = new JScrollPane();
 			scrollPane.setBounds(10, 80, 136, 220);
@@ -208,27 +234,169 @@ public class regEvento extends JDialog {
 			JList list = new JList();
 			scrollPane.setViewportView(list);
 			
-			JLabel lblAreaDeLa = new JLabel("Area de la comision:");
-			lblAreaDeLa.setBounds(10, 315, 122, 14);
-			panel_1.add(lblAreaDeLa);
+			lblAreaDeLaComision = new JLabel("Area de la comision:");
+			lblAreaDeLaComision.setBounds(10, 315, 122, 14);
+			panel_1.add(lblAreaDeLaComision);
 			
-			textField_11 = new JTextField();
-			textField_11.setBounds(10, 330, 441, 20);
-			panel_1.add(textField_11);
-			textField_11.setColumns(10);
+			txtAreaDeComision = new JTextField();
+			txtAreaDeComision.setBounds(10, 330, 441, 20);
+			panel_1.add(txtAreaDeComision);
+			txtAreaDeComision.setColumns(10);
 			
-			JLabel lblNewLabel_6 = new JLabel("Comision");
-			lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 17));
-			lblNewLabel_6.setBounds(10, 160, 82, 14);
-			panel.add(lblNewLabel_6);
+			btnAgregarParticipante = new JButton("Agregar Participante");
+			btnAgregarParticipante.setBounds(158, 241, 191, 23);
+			panel_1.add(btnAgregarParticipante);
+			btnAgregarParticipante.setVisible(false);
+
 			
-			JButton btnNewButton_1 = new JButton("Participantes >");
-			btnNewButton_1.addActionListener(new ActionListener() {
+			btnEliminarParticipante = new JButton("Eliminar Participante");
+			btnEliminarParticipante.setBounds(158, 274, 191, 23);
+			panel_1.add(btnEliminarParticipante);
+			btnEliminarParticipante.setVisible(false);
+			
+
+			
+			btnAgregarRecurso = new JButton("Agregar Recurso");
+			btnAgregarRecurso.setBounds(158, 241, 191, 23);
+			panel_1.add(btnAgregarRecurso);
+			btnAgregarRecurso.setVisible(false);
+
+			
+			btnEliminarRecurso = new JButton("Eliminar Recurso");
+			btnEliminarRecurso.setBounds(158, 274, 191, 23);
+			panel_1.add(btnEliminarRecurso);
+			btnEliminarRecurso.setVisible(false);
+
+			
+			lblTitulo = new JLabel("Comision");
+			lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 17));
+			lblTitulo.setBounds(10, 156, 144, 14);
+			panel.add(lblTitulo);
+			
+			rdbtRecursos = new JRadioButton("Recursos");
+			rdbtRecursos.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
+					
+					
+					btnAgregarRecurso.setVisible(true);
+					btnEliminarRecurso.setVisible(true);
+					
+					btnAgregarParticipante.setVisible(false);
+					btnEliminarParticipante.setVisible(false);
+					
+					btnAgregar.setVisible(false);
+					btnEliminar.setVisible(false);
+					
+					rdbtnComision.setSelected(false);
+					rdbtnParticipantes.setSelected(false);
+					
+					lblEspecialidad.setVisible(false);
+					txtEspecialidad.setVisible(false);
+					
+					btnAgregarComoModerador.setVisible(false);
+					
+					lblAreaDeLaComision.setVisible(false);	
+					txtAreaDeComision.setVisible(false);
+					
+					lblModeradorActual.setVisible(false);
+					txtModeradorActual.setVisible(false);
+					
+					lblBuscar.setText("Buscar recurso por codigo");
+					lblTitulo.setText("Recursos");
+					LBLaCTUALES.setText("Recursos actuales");
+					
+					
+					lblNombreDos.setText("Codigo del Recurso");
+					lblCedula.setText("Nombre");
+					lblTelefono.setText("Tipo");
+					
+					
 				}
 			});
-			btnNewButton_1.setBounds(342, 151, 129, 23);
-			panel.add(btnNewButton_1);
+			rdbtRecursos.setBounds(389, 151, 82, 23);
+			panel.add(rdbtRecursos);
+			
+			rdbtnParticipantes = new JRadioButton("Participantes");
+			rdbtnParticipantes.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+					btnAgregarParticipante.setVisible(true);
+					btnEliminarParticipante.setVisible(true);
+					
+					btnAgregarRecurso.setVisible(true);
+					btnEliminarRecurso.setVisible(true);
+			
+					btnAgregar.setVisible(false);
+					btnEliminar.setVisible(false);
+					
+					
+					
+					rdbtnComision.setSelected(false);
+					rdbtRecursos.setSelected(false);
+					
+					lblEspecialidad.setVisible(false);
+					txtEspecialidad.setVisible(false);
+					
+					btnAgregarComoModerador.setVisible(false);
+					
+					lblAreaDeLaComision.setVisible(false);	
+					txtAreaDeComision.setVisible(false);
+					
+					lblModeradorActual.setVisible(false);
+					txtModeradorActual.setVisible(false);
+					
+					lblBuscar.setText("Buscar participante por cedula");
+					lblTitulo.setText("Participantes");
+					LBLaCTUALES.setText("Participantes actuales");
+				}
+			});
+			rdbtnParticipantes.setBounds(278, 151, 109, 23);
+			panel.add(rdbtnParticipantes);
+			
+			rdbtnComision = new JRadioButton("Comision");
+			rdbtnComision.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+					
+					btnAgregarParticipante.setVisible(false);
+					btnEliminarParticipante.setVisible(false);
+					
+					btnAgregarRecurso.setVisible(false);
+					btnEliminarRecurso.setVisible(false);
+			
+					btnAgregar.setVisible(true);
+					btnEliminar.setVisible(true);
+					
+					
+					rdbtnParticipantes.setSelected(false);
+					rdbtRecursos.setSelected(false);
+					
+					
+					lblEspecialidad.setVisible(true);
+					txtEspecialidad.setVisible(true);
+					
+					btnAgregarComoModerador.setVisible(true);
+					
+					lblAreaDeLaComision.setVisible(true);	
+					txtAreaDeComision.setVisible(true);
+					
+					lblModeradorActual.setVisible(true);
+					txtModeradorActual.setVisible(true);
+					
+					lblBuscar.setText("Buscar jurado por cedula");
+					lblTitulo.setText("Comision");
+					LBLaCTUALES.setText("Jurados actuales");
+					
+					
+					lblNombreDos.setText("Nombre:");
+					lblCedula.setText("Cedula:");
+					lblTelefono.setText("Telefono:");
+					
+				}
+			});
+			rdbtnComision.setSelected(true);
+			rdbtnComision.setBounds(183, 151, 93, 23);
+			panel.add(rdbtnComision);
 		}
 		{
 			JPanel buttonPane = new JPanel();
